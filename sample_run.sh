8af66a457171ead2eaee3b19c50ba318029f1705
jaykhandare <jayendrakhandare@gmail.com>
2021-09-24 17:41:39 +0530

    modified sample run script

--- sample_run.sh
+++ sample_run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 #!/bin/bash
 
+python3 -m pip install --user --upgrade pip 
+
+python3 -m pip install --user virtualenv 
+python3 -m venv env
+
+pip3 install torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements.txt
+
+source env/bin/activate
+
 python3 mlrunner.py train --project_name demo2 --learning_rate 0.02 --dataset ../data/MNIST/processed/training.pt --script ml_algo_module
 python3 mlrunner.py test --project_name demo2 --dataset data/MNIST/processed/testing.pt --script ml_algo_module
 
@@ -18,5 +28,6 @@ python3 mlrunner.py test --project_name demo2 --dataset data/MNIST/processed/tes
 
 
 python3 mlrunner.py report --project_name demo2
-
 python3 mlrunner.py report --project_name demo1
+
+python3 django_ui/manage.py runserver
