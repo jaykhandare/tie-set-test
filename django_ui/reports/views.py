7b9235cbcf199c836f85c86f6db91f81a4b8ee50
jaykhandare <jayendrakhandare@gmail.com>
2021-09-24 21:45:15 +0530

    added comments for clarification

--- django_ui/reports/views.py
+++ django_ui/reports/views.py
@@ -14,6 +14,7 @@ report_dir_location = base_project_dir + "reports/"
 loss_dir_location = base_project_dir + "loss_files/"
 
 
+# this is the current landing page which shows all the projects which are run with the program
 def all_projects(request):
     if request.method == "GET":
         projects = retrieve_all_projects()
@@ -23,11 +24,13 @@ def all_projects(request):
         return render(request, ERROR_404)
 
 
+# this view provides project specific information like details of various runs, etc
 def project_view(request):
     if request.method == "GET":
         project_name = request.GET['project_name']
         print("project view being retrieved for project {}".format(project_name))
-        # retrieve json data for selected project
+
+        # retrieve json data file for selected project
         json_file_path = report_dir_location + project_name + ".json"
         if not os.path.exists(json_file_path):
             return render(request, ERROR_403)
@@ -41,10 +44,12 @@ def project_view(request):
         return render(request, ERROR_404)
 
 
+# this view is called from project view with run_id to show the loss items graph
 def view_loss_graph_for_run(request):
     if request.method == "GET":
         run_id = request.GET['run_id']
         print("loss value graph being retrieved for run_id {}".format(run_id))
+
         # retrieve loss item data for selected run
         loss_file_path = loss_dir_location + run_id + ".csv"
         if not os.path.exists(loss_file_path):
@@ -54,9 +59,11 @@ def view_loss_graph_for_run(request):
             csv_reader = csv.reader(file)
             data_for_loss_graph = list(csv_reader)[0]
 
+        # covert loss items data into percentage to show efficiently in a graph
         data_for_loss_graph = [floor(float(entry) * 100)
                                for entry in data_for_loss_graph]
 
+        # list of numbers to the size of loss items
         points_for_x = [x+1 for x in range(len(data_for_loss_graph))]
 
         layout = {
@@ -76,6 +83,7 @@ def view_loss_graph_for_run(request):
         return render(request, ERROR_404)
 
 
+# this function is called internally from the methods of THIS SCRIPT ONLY
 def retrieve_all_projects():
     print("reports being retrieved...")
     # retrieve file data from report_dir_location and send curated data back
